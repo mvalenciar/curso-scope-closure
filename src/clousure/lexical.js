87870d0d12fe37d8447ccee1e5677305988d3e6e
AlfonsoV92 <miltonalonsovalenciarincon@gmail.com>
2023-02-28 17:51:17 -0500

    Creación de package json y practica scope global, funcion y bloque

---
+++ src/clousure/lexical.js
@@ -0,0 +1,23 @@
+function fruits() {
+  if (true) {
+    var fruit1 = "apple";
+    let fruit2 = "kiwi";
+    const fruit3 = "banana";
+    console.log(fruit2);
+    console.log(fruit3);
+  }
+  console.log(fruit1);
+}
+
+fruits();
+
+const myNumber = [];
+
+myNumber.push(1, 2, 3);
+
+
+console.log(myNumber[0]);
+
+myNumber.pop(3);
+
+console.log(myNumber);
